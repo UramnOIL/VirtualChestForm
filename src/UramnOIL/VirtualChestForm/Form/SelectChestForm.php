a7df1892586f2c0e3b4cda8ca3bf18e0ed2678a7
UramnOIL <uramnoil@outlook.jp>
2019-09-12 18:39:07 +0900

    SelectChestFormの実装.

--- src/UramnOIL/VirtualChestForm/Form/SelectChestForm.php
+++ src/UramnOIL/VirtualChestForm/Form/SelectChestForm.php
@@ -5,14 +5,31 @@ namespace UramnOIL\VirtualChestForm\Form;
 
 
 use Frago9876543210\EasyForms\forms\MenuForm;
+use pocketmine\block\Button;
+use pocketmine\Player;
+use UramnOIL\VirtualChestForm\Controller\SelectChestController;
+use UramnOIL\VirtualChestForm\Form\Button\ChesButton;
 
 class SelectChestForm extends MenuForm {
-	public function __construct() {
+	public function __construct(array $chests, SelectChestController $controller) {
 		$title = "VirtualChestForm";
 		$content = "チェストを選択してください";
-		$buttons = [
+		$buttons = [];
+		foreach($chests as $chest) {
+			$buttons[] = new ChesButton($chest);
+		}
 
-		]
+		$onSubmit = function(Player $player, Button $selected) use ($controller) : void {
+			if(!$selected instanceof ChesButton) {
+				throw new FormException();
+			}
+
+			$controller->onSubmit($player, $selected->getChest());
+		};
+		$onClose = function(Player $player) use ($controller) : void {
+			$controller->onClose($player);
+		};
+		
 		parent::__construct($title, $content, $buttons, $onSubmit, $onClose);
 	}
 }
