c6535b3844751f59e46633480bd9c53691e18c14
UramnOIL <uramnoil@outlook.jp>
2019-09-12 18:05:27 +0900

    VirtualChestEntryFormの実装.

--- src/UramnOIL/VirtualChestForm/Form/VirtualChestEntryForm.php
+++ src/UramnOIL/VirtualChestForm/Form/VirtualChestEntryForm.php
@@ -6,27 +6,30 @@ namespace UramnOIL\VirtualChestForm\Form;
 
 use Frago9876543210\EasyForms\elements\Button;
 use Frago9876543210\EasyForms\forms\MenuForm;
-use UramnOIL\VirtualChestForm\Fomr\Button\EntryButton;
+use pocketmine\Player;
+use UramnOIL\VirtualChestForm\Controller\VirtualChestEntryController;
+use UramnOIL\VirtualChestForm\Form\Button\EntryButton;
 
 class VirtualChestEntryForm extends MenuForm {
 	public const BUTTON_QUIT = 0;
 	public const BUTTON_OPEN_CHEST = 1;
 	public const BUTTON_CUSTOMIZE_CHESTS = 2;
 
-	public function __construct() {
+	public function __construct(VirtualChestEntryController $controller) {
 		$title = "VirtualChestForm";
 		$content = "";
 		$buttons = [
-			new EntryButton(self::BUTTON_QUIT, "戻る"),
 			new EntryButton(self::BUTTON_OPEN_CHEST, "チェストを開く"),
 			new EntryButton(self::BUTTON_CUSTOMIZE_CHESTS, "カスタマイズ"),
 		];
 
-		$onSubmit = function(Button $button) : void {
+		$onSubmit = function(Player $player, Button $button) use ($controller) : void {
 			if(!$button instanceof EntryButton) {
-				throw new
+				throw new FormException();
 			}
+			$controller->onSubmit($player, $button->getId());
 		};
+		$onClose = null;
 
 		parent::__construct($title, $content, $buttons, $onSubmit, $onClose);
 	}
