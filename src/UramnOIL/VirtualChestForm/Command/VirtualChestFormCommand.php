03704811d5295f54beaa06c1de7f156585e5e723
UramnOIL <uramnoil@outlook.jp>
2019-09-12 17:26:52 +0900

    OpenFormCommandの作成.

--- src/UramnOIL/VirtualChestForm/Command/VirtualChestFormCommand.php
+++ src/UramnOIL/VirtualChestForm/Command/VirtualChestFormCommand.php
@@ -4,14 +4,19 @@
 namespace UramnOIL\VirtualChestForm\Command;
 
 
+use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\plugin\Plugin;
 
-class VirtualChestFormCommand extends PluginCommand {
+class OpenFormCommand extends PluginCommand {
 	public function __construct(Plugin $owner) {
 		$name = 'virtualchestform';
 		parent::__construct($name, $owner);
 		$this->setAliases(['vc', 'vcf']);
-		$this->setPermission();
+		$this->setPermission('virtualchestform.command.openform');
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args) {
+		$this->testPermission($sender);
 	}
 }
