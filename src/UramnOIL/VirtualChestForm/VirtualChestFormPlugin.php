6921715afe6134accf96cfc9488022e12565ef2e
UramnOIL <uramnoil@outlook.jp>
2019-09-12 19:59:53 +0900

    getFormFactory()の実装.

--- src/UramnOIL/VirtualChestForm/VirtualChestFormPlugin.php
+++ src/UramnOIL/VirtualChestForm/VirtualChestFormPlugin.php
@@ -3,7 +3,21 @@
 namespace UramnOIL\VirtualChestForm;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use UramnOIL\VirtualChestForm\Form\FormFactory;
+use uramnoil\virtualinventory\VirtualInventoryPlugin;
 
 class VirtualChestFormPlugin extends PluginBase {
+	/** @var FormFactory */
+	private $factory;
 
+	public function onEnable() {
+		/** @var VirtualInventoryPlugin $plugin */
+		$plugin = Server::getInstance()->getPluginManager()->getPlugin('VirtualInventoryPlugin');
+		$this->factory = new FormFactory($plugin->getAPI());
+	}
+
+	public function getFormFactory() : FormFactory {
+		return $this->factory;
+	}
 }
