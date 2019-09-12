20f73ffd2f682ab5080b96a518167338c4d59111
UramnOIL <uramnoil@outlook.jp>
2019-09-12 18:39:48 +0900

    SelectChestControllerの実装.

--- src/UramnOIL/VirtualChestForm/Controller/SelectChestController.php
+++ src/UramnOIL/VirtualChestForm/Controller/SelectChestController.php
@@ -5,10 +5,15 @@ namespace UramnOIL\VirtualChestForm\Controller;
 
 
 use pocketmine\Player;
+use UramnOIL\VirtualChestForm\Form\VirtualChestEntryForm;
 use uramnoil\virtualinventory\inventory\VirtualChestInventory;
 
 class SelectChestController {
 	public function onSubmit(Player $player, VirtualChestInventory $chest) : void {
 		$player->addWindow($chest);
+	}
+
+	public function onClose(Player $player) : void {
+		$player->sendForm(new VirtualChestEntryForm());
 	}
 }
