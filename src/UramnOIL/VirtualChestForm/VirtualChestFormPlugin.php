4a8d62c15d705eb1c695ef82b71d93a17d7c6dcb
UramnOIL <uramnoil@outlook.jp>
2019-09-13 22:29:10 +0900

    リファクタリング.

--- src/UramnOIL/VirtualChestForm/VirtualChestFormPlugin.php
+++ src/UramnOIL/VirtualChestForm/VirtualChestFormPlugin.php
@@ -4,6 +4,7 @@ namespace UramnOIL\VirtualChestForm;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use UramnOIL\VirtualChestForm\Command\EntryFormCommand;
 use UramnOIL\VirtualChestForm\Form\FormFactory;
 use uramnoil\virtualinventory\VirtualInventoryPlugin;
 
@@ -15,6 +16,7 @@ class VirtualChestFormPlugin extends PluginBase {
 		/** @var VirtualInventoryPlugin $plugin */
 		$plugin = Server::getInstance()->getPluginManager()->getPlugin('VirtualInventoryPlugin');
 		$this->factory = new FormFactory($plugin->getAPI());
+		$this->getServer()->getCommandMap()->register("VirtualChestForm", new EntryFormCommand($this));
 	}
 
 	public function getFormFactory() : FormFactory {
